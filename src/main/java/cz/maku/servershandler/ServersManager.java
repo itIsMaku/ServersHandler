6de335ebb97c13de2726331640d509377d9b8a9e
Adam Volkman <itismaku@gmail.com>
2021-05-16 14:01:16 +0200

    feat: added API methods

--- src/main/java/cz/maku/servershandler/ServersManager.java
+++ src/main/java/cz/maku/servershandler/ServersManager.java
@@ -1,12 +1,25 @@
 package cz.maku.servershandler;
 
+import cz.maku.servershandler.jedis.JedisHandler;
 import cz.maku.servershandler.servers.Server;
+
+import java.util.Map;
 
 public class ServersManager {
 
-    public static Server getServer() {
-        return Instance.getInstance().getServersHandler().getServer();
+    public static ServersHandler getServersHandler() {
+        return Instance.getInstance().getServersHandler();
     }
 
+    public static Server getServer() {
+        return getServersHandler().getServer();
+    }
 
+    public static Map<String, Server> getServers() {
+        return getServersHandler().getServers();
+    }
+
+    public static JedisHandler getJedisHandler() {
+        return getServersHandler().getJedisHandler();
+    }
 }
